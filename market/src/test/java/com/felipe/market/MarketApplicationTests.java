de728beb522c78a5d60fbf535dec18c8ee97cf2d
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-17 16:55:27 -0500

    [CREATED PROJECT MARKET]
    Creamos el proyecto con Spring.

---
+++ market/src/test/java/com/felipe/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
